dedb50f4734b9a3975f108b73adaa26a0ffac67f
Than Van Ky <thanky4444.com>
2025-03-07 22:17:42 +0700

    initial

--- index.php
+++ index.php
@@ -1,6 +1,27 @@
-<?
-    $Name = 'Ky';
-    $food = 'chicken';
-    echo "Name is: {$Name} <br>";
-    echo "The food {$Name} likes is {$food}";
-?>  
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+   <form action= "index.php" method="get">
+        <input type="text" name="userName" placeholder="User Name"><br>
+        <input type="password" name="password"placeholder="Password"><br>
+        <input type="submit" value="Log in" name="submit">   
+   </form>
+</body>
+</html>
+
+<?php
+
+    if($_GET["userName"] == "thanky" &&  $_GET["password"] == 123456)
+    {
+        echo "corret";
+    }
+    else{
+        echo "User Name or password isn't math ";
+    }
+
+?>
